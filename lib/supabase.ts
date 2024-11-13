319421b7b4275f6524f7474754713f89e974dbf3
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-11-14 01:35:05 +0530

    Configured Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,30 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseKey = process.env.SUPABASE_KEY as string
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function getBlogById(id: number) {
+  const { data, error } = await supabase
+    .from('blogs')
+    .select()
+    .eq('id', id)
+    .single()
+  if (error) {
+    console.error("Error fetching blog by ID:", error)
+    return null
+  }
+  return data
+}
+
+export async function getAllBlogs() {
+  const { data, error } = await supabase
+    .from('blogs')
+    .select()
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.error("Error fetching all blogs:", error)
+    return null
+  }
+  return data
+}
