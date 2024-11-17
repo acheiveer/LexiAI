aad08538a95e3d707394be21e9d2c46462583dd5
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-11-18 01:36:38 +0530

    blog with image created successfully and saved to DB

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+        {protocol: 'https',
+    hostname: process.env.SUPABASE_HOSTNAME as string}
+    ]
+}
 };
 
 export default nextConfig;
